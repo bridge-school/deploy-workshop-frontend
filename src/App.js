a16deb26ec74a65e5b4f801207d31b519a22af4f
Purvi Kanal <purvikanal92@gmail.com>
2019-10-24 13:58:48 -0400

    my second feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi I wrote a feature!
+          Hi I wrote a feature! And then I wrote _another_ feature!
         </p>
         <a
           className="App-link"
